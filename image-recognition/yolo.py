02cecb1d412b380e34300a4be981d7e8d52a0959
Dmitry <grashin2011@gmail.com>
2021-12-15 13:35:28 +0300

    Bug fixes

--- image-recognition/yolo.py
+++ image-recognition/yolo.py
@@ -1,12 +1,5 @@
-# USAGE
-# python yolo.py --image images/baggage_claim.jpg --yolo yolo-coco
-
-# import the necessary packages
 import numpy as np
-import argparse
-import time
 import cv2
-import os
 
 
 
@@ -17,18 +10,14 @@ def init_yolo(configPath, weightsPath):
 	return net
 
 	
-def recognize_image(input_file, output_file, labelsPath, net)
+def recognize_image(input_file, output_file, labelsPath, net):
 
-	confidence = 0.5
+	confidence_value = 0.5
 	threshold = 0.3
 
-	
-	# load our input image and grab its spatial dimensions
-	image = cv2.imread(args["image"])
+	image = cv2.imread(input_file)
 	(H, W) = image.shape[:2]
 
-	# determine only the *output* layer names that we need from YOLO
-	
 	blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
 		swapRB=True, crop=False)
 	net.setInput(blob)
@@ -44,50 +33,37 @@ def recognize_image(input_file, output_file, labelsPath, net)
 	confidences = []
 	classIDs = []
 
-	# loop over each of the layer outputs
 	for output in layerOutputs:
-		# loop over each of the detections
+
 		for detection in output:
-			# extract the class ID and confidence (i.e., probability) of
-			# the current object detection
+
 			scores = detection[5:]
 			classID = np.argmax(scores)
 			confidence = scores[classID]
 
-			# filter out weak predictions by ensuring the detected
-			# probability is greater than the minimum probability
-			if confidence > confidence:
-				# scale the bounding box coordinates back relative to the
-				# size of the image, keeping in mind that YOLO actually
-				# returns the center (x, y)-coordinates of the bounding
-				# box followed by the boxes' width and height
+			if confidence > confidence_value:
+
 				box = detection[0:4] * np.array([W, H, W, H])
 				(centerX, centerY, width, height) = box.astype("int")
 
-				# use the center (x, y)-coordinates to derive the top and
-				# and left corner of the bounding box
 				x = int(centerX - (width / 2))
 				y = int(centerY - (height / 2))
 
-				# update our list of bounding box coordinates, confidences,
-				# and class IDs
 				boxes.append([x, y, int(width), int(height)])
 				confidences.append(float(confidence))
 				classIDs.append(classID)
 
 
-	idxs = cv2.dnn.NMSBoxes(boxes, confidences, confidence,
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, confidence_value,
 		threshold)
 
-	# ensure at least one detection exists
 	if len(idxs) > 0:
-		# loop over the indexes we are keeping
+
 		for i in idxs.flatten():
-			# extract the bounding box coordinates
+
 			(x, y) = (boxes[i][0], boxes[i][1])
 			(w, h) = (boxes[i][2], boxes[i][3])
 
-			# draw a bounding box rectangle and label on the image
 			color = [int(c) for c in COLORS[classIDs[i]]]
 			cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
 			text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
@@ -95,6 +71,7 @@ def recognize_image(input_file, output_file, labelsPath, net)
 				0.5, color, 2)
 
 	cv2.imwrite(output_file, image)
-	#return image
 
-
+# usage
+# net = init_yolo('yolov3.cfg', 'yolov3.weights')
+# recognize_image('inp.png', 'out.png', 'coco.names', net)
